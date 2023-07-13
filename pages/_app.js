18425159ef8923a685ecf3fd3df185a4856ab964
Zbynek Svoboda <wentura17@seznam.cz>
2023-07-13 13:10:20 +0200

    trochu seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
+import Head from "next/head";
 import Footer from "/components/footer";
 import "/public/globals.css";
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="author" content="www.zbyneksvoboda.cz" />
+        <meta name="copyright" content="2023 Autokemp Kokořín" />
+        <meta name="robots" content="index,follow" />
+        <meta
+          name="keywords"
+          content="kokořín,ubytování v přírodě,kempy,kokořínsko,škola v přírodě,teambuilding,srazy,rodinná dovolená, kokořín, autokemp, kemp"
+        />
+        <meta
+          name="description"
+          content="Autokemp Kokořín ubytování v nádherném údolí Kokořínska"
+        />
+      </Head>
       {/* <Header /> */}
       <Component {...pageProps} />
       <Footer />
