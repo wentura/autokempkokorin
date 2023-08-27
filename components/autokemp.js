b4858d93eed2350f6dcc764126fa850cb0505b9b
Zbynek Svoboda <wentura17@seznam.cz>
2023-08-27 16:49:42 +0200

    skryti aktualit

--- components/autokemp.js
+++ components/autokemp.js
@@ -13,7 +13,7 @@ export default function Autokemp() {
         />
       </Head>
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="px-4 py-4 mb-24 mx-auto max-w-full sm:max-w-5xl border-0 border-l-4 border-gray-400">
+        {/* <div className="px-4 py-4 mb-24 mx-auto max-w-full sm:max-w-5xl border-0 border-l-4 border-gray-400">
           <h3 className=" mx-auto font-sans font-bolder ctaHeader text-2xl mb-4">
             Aktuality autokempu Kokořín
           </h3>
@@ -25,7 +25,7 @@ export default function Autokemp() {
               Děkujeme za pochopení
             </li>
           </ul>
-        </div>
+        </div> */}
         <div className="grid gap-10 lg:grid-cols-2">
           <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
             <div className="max-w-xl mb-6 font-extralight">
