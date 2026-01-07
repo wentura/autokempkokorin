e0a1958fe0d47ba4bc5a137f5b7568d26aef030a
wentura <svoboda.zbynek@gmail.com>
2026-01-07 12:23:45 +0100

    app router a skryty cenik

--- next.config.js
+++ next.config.js
@@ -16,6 +16,7 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    qualities: [75, 85], // Support both default 75 and our custom 85
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
